0eda31818be4ad4199e7110a47f2f58665d95e77
xXLATE <bruno-roberto@mail.ru>
2022-03-21 21:55:03 +0300

    initial project structure

--- ClickerClicker/ClickerClicker/Pages/RegistrationPage.xaml.cs
+++ ClickerClicker/ClickerClicker/Pages/RegistrationPage.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace ClickerClicker
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClickerClicker.Pages
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
     {
-        public MainPage()
+        public RegistrationPage()
         {
             InitializeComponent();
         }
