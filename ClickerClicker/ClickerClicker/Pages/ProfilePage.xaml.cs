c1bd721665cf4131226de9064a10705d638d75bd
LalPotRash <daniil.kanev1994@gmail.com>
2022-03-22 01:48:25 +0300

    add profile page

---
+++ ClickerClicker/ClickerClicker/Pages/ProfilePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClickerClicker.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnBackButtonPressed(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
